6f94ce194d94d6d423a57be7a7bf847c0a9b80ab
Azwad Aziz <azwad.aziz99@outlook.com>
2023-12-05 02:13:26 +0600

    Admin contents

---
+++ Admin/Create_Membership/base.php
@@ -0,0 +1,79 @@
+<?php
+$id= $_GET['id'];
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Membership profiles</title>
+    <link rel="stylesheet" href="base.css">
+    <div class="containerx">
+            
+            <div class="row">
+                  
+                  <div class="delete" onclick="redirectToPage1()">
+                        <h2>Create Faculty Profile</h2>
+                        
+                  </div>
+                  <div class="delete" onclick="redirectToPage2()">
+                        <h2>Create Student Profile</h2>
+                        
+                  </div>
+
+    
+             
+            </div>
+
+
+
+      </div>
+ 
+      <script>
+        var jsVariable = <?php echo json_encode($id); ?>;
+    // JavaScript function to handle the redirection
+    function redirectToPage1() {
+      // Change the URL to the desired destination
+      window.location.href = './faculty_profile.php?id='+jsVariable; // Replace 'https://example.com' with your actual URL
+    }
+    function redirectToPage2() {
+      // Change the URL to the desired destination
+      window.location.href = './student_profile.php?id='+jsVariable; // Replace 'https://example.com' with your actual URL
+    }
+    function redirectToPage3() {
+      // Change the URL to the desired destination
+      window.location.href = './admin_profile.php?id='+jsVariable; // Replace 'https://example.com' with your actual URL
+    }
+    function redirectToPage4() {
+      // Change the URL to the desired destination
+      window.location.href = './cook_profile.php?id='+jsVariable; // Replace 'https://example.com' with your actual URL
+    }
+  </script>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Admin/Update_Menu/update_menu.php
@@ -0,0 +1,152 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Update Menu</title>
+     
+      <link rel="stylesheet" href="update_menu.css">
+</head>
+<body>
+
+      <main class="table">
+        <section class="table__header">
+            <h1>Menu</h1>
+        </section>
+        <section class="table__body">
+            <table>
+                <thead>
+                    <tr>
+                        <th> Item </th>
+                        <th> Price </th>
+                        <th> Quantity </th>
+                        
+                        <th> New Quantity</th>
+                        <th> New Price</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+  
+
+                    <?php
+                    include '../../dbconnect.php';
+    
+                    $sql = "Select * from menu;";
+                    $result = mysqli_query($conn, $sql);
+                    $temp = 1;
+                    while ($row = mysqli_fetch_assoc($result)){
+                        
+                        $item_name = $row['Name'];
+                        $price = $row['Price'];
+                        $quantity = $row['Quantity'];
+                        
+                        
+                        echo "<script>";
+                        echo "addToDictionary(".$item_name.");";
+                        echo "</script>";
+                   
+                        echo "
+                        <tr>
+                        <td>". $item_name."</td> 
+                        <td>". $price."</td> 
+                        <td>". $quantity."</td>
+                        
+                        <td>
+
+                        <button type='button' onclick='decreaseQuantity($temp)'>-</button>
+                        <span  id='quantity_".$temp."'>0</span>
+                        <button type='button' onclick='increaseQuantity($temp)'>+</button>
+                        </td>
+
+                        <td>  <input type='textr' id=".$temp." data-custom-attribute=".$temp." oninput='handleInput(this.value,this.dataset.customAttribute)' placeholder=''>  </td>
+                        </tr>";
+                        
+
+                        
+                        $temp = $temp +1;
+                    
+                    }
+?>
+       
+                </tbody>
+            </table>
+
+
+        </section>
+
+
+
+
+
+
+
+    </main>
+        <main class="table2"> 
+        <section class="table__header2">
+            <h1>Add/Delete an Item</h1>
+        </section>
+        <section class="table__body2">
+            <table>
+                <thead>
+                    <tr>
+                        <th> Item Name </th>
+                        <th> Price </th>
+                        <th> Health Factor </th>
+              
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td><input type="texty" id='p1' placeholder=""></td>
+                    <td><input type="texty" id='p2' placeholder=""></td>
+                    <td><input type="texty"  id='p3' placeholder=""></td>
+                
+                
+                </tr>
+                
+
+       
+                </tbody>
+            </table>
+
+
+        </section>
+    </main>
+    <p id="output1"></p>
+
+
+
+
+    
+
+    
+    
+
+
+ <button type = 'submit' class='styled-button_2' onclick='concatenatePositiveValues(); redirectToPage2()'>Approve</button>
+ <button type = 'submit' class='styled-button_3' onclick='redirectToPage3()'> Insert Item</button>
+ <button type = 'submit' class='styled-button' onclick='redirectToPage()' >Delete Item</button>
+        
+ 
+ 
+ <script src="./update_menu_script.js"></script>
+        <script>
+        // Use PHP echo to output the PHP variable as a JavaScript variable
+        var jsVariable = <?php echo json_encode($_GET['id']); ?>;
+        // Now you can use jsVariable in your external JavaScript file
+    </script>
+      <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+      <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
+
+
+
+
+
+
